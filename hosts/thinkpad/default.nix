4377ff05d3981aa0d7b890528ed66cce4721572c
henrym <henrydmoore23@gmail.com>
2025-06-05 15:38:01 -0700

    merges default.nix and configuration.nix

--- hosts/thinkpad/default.nix
+++ hosts/thinkpad/default.nix
@@ -1,8 +1,7 @@
 { pkgs, ... }:
 {
   imports = [
-    ./configuration.nix 
-    ./hardware-configuration.nix
+    ./hardware.nix
   ];
 
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
@@ -18,6 +17,13 @@
   programs = {
     hyprland.enable = true;
     zsh.enable = true;
+    mtr.enable = true;
+    git.enable = true;
+
+    gnupg.agent = {
+      enable = true;
+      enableSSHSupport = true;
+    };
   };
 
   services = {
@@ -33,6 +39,16 @@
 	default_session.user = "henrym";
       };
     };
+
+    resolved.enable = true;
+    thermald.enable = true;
+    tlp.enable = true;
+    openssh.enable = true;
+
+    pipewire = {
+      enable = true;
+      pulse.enable = true;
+    };
   };
 
   systemd.services.greetd.serviceConfig = {
@@ -44,4 +60,29 @@
     TTYVHangup = true;
     TTYVTDisallocate = true;
   };
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "thinkpad";
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "America/Los_Angeles";
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  powerManagement.enable = true;
+
+  users.users.henrym = {
+    isNormalUser = true;
+    home = "/home/henrym";
+    shell = pkgs.zsh;
+    extraGroups = [ "wheel" "networkmanager" ]; # Enable ‘sudo’ for the user.
+    packages = with pkgs; [
+      tree
+    ];
+  };
+
+  system.stateVersion = "24.11";
 }
+
